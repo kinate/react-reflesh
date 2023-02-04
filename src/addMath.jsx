9041fb38b4c37081a91bf7495c5247b05aca8be8
kinate <kinate82@gmail.com>
2023-02-04 22:18:47 +0300

    - Initial CRA setup - Import/Export modules

---
+++ src/addMath.jsx
@@ -0,0 +1,6 @@
+function add(a,b){
+    const sum = a+b;
+    return sum;
+}
+
+export default add
---
+++ src/users.jsx
@@ -0,0 +1,14 @@
+const user = {
+
+    name:'sabuni',
+    age:20,
+    hobie:['sing','dance','code']
+}
+
+export const locations = {
+    location1:'Arusha',
+    location2:'Dar',
+    location2:'Mbeya',
+}
+
+export default user
